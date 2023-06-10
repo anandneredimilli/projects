e81fa460983826789dbbceea0acdf1f4269e8898
anandneredimilli <paulanand926@gmail.com>
2023-06-10 19:55:39 +0530

    solved exceptions and added search function

--- Password_Manager/main.py
+++ Password_Manager/main.py
@@ -1,28 +1,61 @@
 import tkinter
 import random
 from tkinter import messagebox
-
+import json
 
 
 window = tkinter.Tk()
 window.title("Password Manager")
 window.config(padx=50,pady=50)
 
-#-----------------function to add data to text file----------------------#
+# #-----------------function to add data to text file----------------------#
+# def add_to_file():
+#     website = website_entry.get()
+#     username = username_entry.get()
+#     password = password_entry.get()
+
+#     if len(website) ==0 or len(username)==0 or len(password)==0:
+#         messagebox.showwarning(title="left fields empty", message='u left some fields empty')
+#     else:
+#         message = messagebox.askokcancel(title="entered details", 
+#                                      message=f'these are the entered credentials for {website} , username:{username} and password:{password}')
+#         if message:
+#             with open(file="Python/Tkinter/Password_Manager/passwords.txt", mode='a') as passwords_file:
+#                 passwords_file.write( f"{website} ' | ' {username} ' | ' {password} \n")
+
+
+#-----------fuction to add data to json file--------------#
 def add_to_file():
     website = website_entry.get()
     username = username_entry.get()
     password = password_entry.get()
 
+    credentials_dict = {
+        website:{
+            'username': username,
+            'password':password
+        }
+    }
     if len(website) ==0 or len(username)==0 or len(password)==0:
         messagebox.showwarning(title="left fields empty", message='u left some fields empty')
     else:
         message = messagebox.askokcancel(title="entered details", 
                                      message=f'these are the entered credentials for {website} , username:{username} and password:{password}')
         if message:
-            with open(file="Python/Tkinter/Password_Manager/passwords.txt", mode='a') as passwords_file:
-                passwords_file.write( f"{website} ' | ' {username} ' | ' {password} \n")
-
+            try:
+                with open(file="credentials.json", mode='r') as credentials:
+                    data = json.load(credentials)
+                    print(data)
+                    data.update(credentials_dict)
+            except FileNotFoundError:
+                #writing into json
+                with open(file="credentials.json", mode='w') as credentials:
+                    json.dump(credentials_dict,credentials ,indent=1)
+            else:
+                with open(file="credentials.json", mode='w') as credentials:
+                    json.dump(data,credentials ,indent=1)
+            
+#-------- func to clear the entered data in entries-------#
 def clear():
     website_entry.delete(0, tkinter.END)
     password_entry.delete(0, tkinter.END)
@@ -52,18 +85,40 @@ def generate_password():
     password_entry.clipboard_append(shuffled_password)
     '''import pyperclip
        pyperclip.copy("thing to be copied")''' #other way to copy the generated password to clipboard
+
+
+#-------------------function to search data inside the json---------------#
+def search():
+    item_to_search = website_entry.get()
+
+    try:
+        with open(file="credentials.json", mode='r') as data:
+            read_data = json.load(data)
+    except FileNotFoundError:
+        messagebox.showwarning(title="no file found", message="no data file found")    
+    else:
+        try:
+            if item_to_search in read_data:
+                    username_found = read_data[item_to_search]['username']
+                    password_found = read_data[item_to_search]['password']
+                    messagebox.showinfo(title="found", message=f"username:{username_found} \n password:{password_found}")
+        except:
+            messagebox.showwarning(title="no data found", message=f"no data found regarding {item_to_search}")    
+#-------------UI--------------------#
 canvas = tkinter.Canvas(height=200,width=189)
-image1 = tkinter.PhotoImage(file='Python/Tkinter/Password_Manager/logo.png')
+image1 = tkinter.PhotoImage(file='logo.png')
 canvas.create_image(100,95.5, image=image1)
 canvas.grid(row=0,column=1)
 
 website_label = tkinter.Label(text="Website Url:" , font=("consolas",12))
 website_label.grid(row=1, column=0)
 
-website_entry =  tkinter.Entry(width=50)
-website_entry.grid(row=1,column=1,columnspan=2)
+website_entry =  tkinter.Entry(width=30)
+website_entry.grid(row=1,column=1)
 website_entry.focus()
 
+search_button = tkinter.Button(text="search",width=15,command=search)
+search_button.grid(row=1,column=2)
 username_label = tkinter.Label(text='Username:', font=("consolas",12))
 username_label.grid(row=2, column=0)
 
